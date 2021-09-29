72a49d8e8bafc77f899da7fa4d20e28a692ac898
harisov-mobile <v8spec@mail.ru>
2021-09-29 14:19:40 +0300

    Adding entities annotation

--- app/src/main/java/ru/internetcloud/tasks/Task.kt
+++ app/src/main/java/ru/internetcloud/tasks/Task.kt
@@ -1,12 +1,21 @@
 package ru.internetcloud.tasks
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
 import ru.internetcloud.tasks.data.model.TaskCategory
 
 @Entity
 data class Task(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
+
     val title: String,
+
     val isCompleted: Boolean,
+
+    @ColumnInfo(name = "category")
+    @TypeConverters()
     val category: TaskCategory
 )
